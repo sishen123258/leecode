e8ee230675c120200f7e9c0cd552485d6a138dec
tong.yue <tong.yue@hypers.com>
2020-07-12 23:21:16 +0800

    inverse tree

---
+++ tree/src/main/java/InverseBT_226/Solution.java
@@ -0,0 +1,25 @@
+package InverseBT_226;
+
+import base.TreeNode;
+
+public class Solution {
+
+
+    public TreeNode invertTree(TreeNode root) {
+
+        if(root==null){
+            return null;
+        }
+
+        TreeNode left=invertTree(root.left);
+
+        TreeNode right = invertTree(root.right);
+
+        root.left=right;
+        root.right=left;
+
+        return root;
+    }
+
+
+}
