3081f4366c1f3ad910a8b02835e2330b1a1da12f
tong.yue <tong.yue@hypers.com>
2020-08-08 22:43:35 +0800

    Frog graph

--- graph/src/main/java/graph/FrogGraph.java
+++ graph/src/main/java/graph/FrogGraph.java
@@ -1,58 +1,142 @@
 package graph;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class FrogGraph {
 
-    private List<Integer> series;
 
-    private Comparator<Integer> comparator = new Comparator<Integer>() {
-        @Override
-        public int compare(Integer o1, Integer o2) {
-            return o2 - o1;
-        }
-    };
+    private List<Node> nodes;
 
-    public FrogGraph(List<Integer> series) {
-        this.series = series;
+    boolean flag = true;
+
+    private Integer[][] matrix;
+
+    private Integer n;
+
+
+    public FrogGraph() {
     }
 
-    public boolean havelHakimi(List<Integer> nums){
-        if(nums.size()<=0){
-            return true;
-        }
-        nums.sort(comparator);
 
-        Integer first = nums.get(0);
+    private void initMatrix() {
 
-        if(first==0){
-            return true;
-        }
+        matrix = new Integer[n][n];
+    }
 
-        if(first>nums.size()-1){
-            return false;
-        }
+    public void setNodes(List<Node> nodes) {
+        this.nodes = nodes;
+    }
 
-        List<Integer> arrs=new ArrayList<>();
-        for(int i=1;i<nums.size();i++){
-            Integer value = nums.get(i);
-            value-=1;
-            if(value<0){
-                return false;
+    public void setN(Integer n) {
+        this.n = n;
+    }
+
+    private void buildGraph() {
+
+        //Sort
+        //first=n
+        //后续n个点度数-1
+        //判断能不能构成图
+        //记录到matrix里面
+
+        int i = 0;
+        List<Node> tmpNodes = this.nodes.subList(i, n);
+
+
+        for (i = 0; i < n & flag; i++) {
+
+
+            Collections.sort(tmpNodes);
+
+            Node firstNode = tmpNodes.get(0);
+
+            if (firstNode.degree > tmpNodes.size() - 1) {
+                flag = false;
             }
-            arrs.add(value);
+
+            for (int j = 1; j <= firstNode.degree & flag; j++) {
+
+                Node nextJNode = tmpNodes.get(j);
+
+                if (nextJNode.degree <= 0) {
+                    flag = false;
+                }
+
+                nextJNode.degree -= 1;
+
+                matrix[firstNode.id-1][nextJNode.id-1] = matrix[nextJNode.id-1][firstNode.id-1] = 1;
+            }
+
+
+            tmpNodes = tmpNodes.subList(1, tmpNodes.size());
         }
 
-        return havelHakimi(arrs);
+        if (flag) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int r = 0; r < n; r++) {
+                for (int j = 0; j < n; j++) {
+                    Integer obj = matrix[r][j];
+                    if (obj == null) {
+                        sb.append(0);
+                    } else {
+                        sb.append(obj);
+                    }
+                    sb.append(",");
+                }
+                sb.append("\n");
+            }
+            System.out.println(sb);
+        }
+
     }
+
+    class Node implements Comparable<Node> {
+        Integer id;
+        Integer degree;
+
+        public Node(Integer id, Integer degree) {
+            this.id = id;
+            this.degree = degree;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return o.degree - this.degree;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "id=" + id +
+                    ", degree=" + degree +
+                    '}';
+        }
+    }
+
 
     public static void main(String[] args) {
-        FrogGraph frogGraph=new FrogGraph(Arrays.asList(2,1,1));
 
-        boolean b = frogGraph.havelHakimi(frogGraph.series);
-        System.out.println("b = " + b);
+        List<Node> nodes = new ArrayList<>();
+
+
+        FrogGraph frogGraph = new FrogGraph();
+
+        nodes.add(frogGraph.new Node(1, 2));
+        nodes.add(frogGraph.new Node(2, 1));
+        nodes.add(frogGraph.new Node(3, 1));
+        nodes.add(frogGraph.new Node(4, 2));
+
+        frogGraph.setNodes(nodes);
+        frogGraph.setN(nodes.size());
+        frogGraph.initMatrix();
+
+        frogGraph.buildGraph();
 
 
     }
+
 
 }
---
+++ graph/src/main/java/graph/HavelHakimi.java
@@ -0,0 +1,58 @@
+package graph;
+
+import java.util.*;
+
+public class HavelHakimi {
+
+    private List<Integer> series;
+
+    private Comparator<Integer> comparator = new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o2 - o1;
+        }
+    };
+
+    public HavelHakimi(List<Integer> series) {
+        this.series = series;
+    }
+
+    public boolean havelHakimi(List<Integer> nums){
+        if(nums.size()<=0){
+            return true;
+        }
+        nums.sort(comparator);
+
+        Integer first = nums.get(0);
+
+        if(first==0){
+            return true;
+        }
+
+        if(first>nums.size()-1){
+            return false;
+        }
+
+        List<Integer> arrs=new ArrayList<>();
+        for(int i=1;i<nums.size();i++){
+            Integer value = nums.get(i);
+            value-=1;
+            if(value<0){
+                return false;
+            }
+            arrs.add(value);
+        }
+
+        return havelHakimi(arrs);
+    }
+
+    public static void main(String[] args) {
+        HavelHakimi frogGraph=new HavelHakimi(Arrays.asList(2,1,1));
+
+        boolean b = frogGraph.havelHakimi(frogGraph.series);
+        System.out.println("b = " + b);
+
+
+    }
+
+}
