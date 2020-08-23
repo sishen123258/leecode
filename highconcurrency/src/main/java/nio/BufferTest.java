8df8645be30e85a8c55ea07e8bda2b098232bf06
TONG <1144299328@qq.com>
2020-08-23 14:51:16 +0800

    add HC

---
+++ highconcurrency/src/main/java/nio/BufferTest.java
@@ -0,0 +1,45 @@
+package nio;
+
+import java.nio.IntBuffer;
+
+public class BufferTest {
+
+    public static void main(String[] args) {
+
+        IntBuffer intBuffer=IntBuffer.allocate(10);
+        int position = intBuffer.position();
+        int limit = intBuffer.limit();
+        int capacity = intBuffer.capacity();
+
+        System.out.println("capacity = " + capacity);
+        System.out.println("limit = " + limit);
+        System.out.println("position = " + position);
+
+        for (int i = 0; i < 5; i++) {
+            intBuffer.put(i);
+        }
+
+        position = intBuffer.position();
+        limit = intBuffer.limit();
+        capacity = intBuffer.capacity();
+
+        System.out.println("\n");
+        System.out.println("capacity = " + capacity);
+        System.out.println("limit = " + limit);
+        System.out.println("position = " + position);
+
+        intBuffer.flip();
+
+        position = intBuffer.position();
+        limit = intBuffer.limit();
+        capacity = intBuffer.capacity();
+
+        System.out.println("\n");
+        System.out.println("capacity = " + capacity);
+        System.out.println("limit = " + limit);
+        System.out.println("position = " + position);
+
+
+
+    }
+}
---
+++ tree/src/test/java/Test.java
@@ -0,0 +1,11 @@
+import com.amazonaws.services.kms.AWSKMS;
+import com.amazonaws.services.kms.AWSKMSClientBuilder;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
